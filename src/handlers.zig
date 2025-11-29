fbb7ba93e9a045ea331c0dd9b266f937159dced0
Nickita Biryulin <keddad@yandex.ru>
2025-11-29 22:52:38 +0300

    Working SaaS

--- src/handlers.zig
+++ src/handlers.zig
@@ -3,10 +3,32 @@ const std = @import("std");
 
 pub const App = struct {};
 
-pub fn handleSleepDouglas(app: *App, req: *httpz.Request, res: *httpz.Response) !void {
-    std.Thread.sleep(std.time.ns_per_s * 42);
+pub fn handleRequest(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+    const time = req.param("time");
 
-    _ = app;
-    _ = req;
-    _ = res;
+    if (time == null) {
+        res.status = 400;
+        res.body = "Time to wait not set";
+        return;
+    }
+
+    const timeInt = std.fmt.parseInt(u64, time.?, 10) catch |err| {
+        res.status = 400;
+        res.body = try std.fmt.allocPrint(res.arena, "Invalid time format: {s}", .{@errorName(err)});
+        return;
+    };
+
+    const timeMultiplier: *const u64 = @ptrCast(@alignCast(req.route_data));
+
+    if (timeInt * timeMultiplier.* > 5 * std.time.ns_per_min) {
+        res.status = 400;
+        res.body = "To wait for times longer then 5 minutes, subscribe too Sleep-As-A-Service Pro Max";
+        return;
+    }
+
+    std.Thread.sleep(timeInt * timeMultiplier.*);
+}
+
+pub fn handleSleepDouglas(_: *App, _: *httpz.Request, _: *httpz.Response) !void {
+    std.Thread.sleep(std.time.ns_per_s * 42);
 }
--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,19 @@ pub fn main() !void {
 
     router.get("/api/v1/sleep/douglas", handlers.handleSleepDouglas, .{});
 
+    const ns_per_s = @as(u64, std.time.ns_per_s);
+    const ns_per_ms = @as(u64, std.time.ns_per_ms);
+    const ns_per_min = @as(u64, std.time.ns_per_min);
+    router.get("/api/v1/sleep/s/:time", handlers.handleRequest, .{
+        .data = &ns_per_s,
+    });
+    router.get("/api/v1/sleep/ms/:time", handlers.handleRequest, .{
+        .data = &ns_per_ms,
+    });
+    router.get("/api/v1/sleep/min/:time", handlers.handleRequest, .{
+        .data = &ns_per_min,
+    });
+
     try std.fs.File.stdout().writeAll("saas ready to receive connections on port 8080");
 
     try server.listen();
